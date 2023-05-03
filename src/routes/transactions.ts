2b2b169a9b1629961382f7818760c4a654a2b18c
brunoper3s <brunoperes7@hotmail.com>
2023-05-03 09:30:53 -0300

    Update Get Transactions and Create List Transaction by id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -24,8 +24,24 @@ export async function transactionsRoute(app: FastifyInstance) {
     return reply.status(201).send()
   })
 
-  app.get('/', async (request, reply) => {
+  app.get('/', async () => {
     const transactions = await knex('transactions')
-    return reply.send(transactions)
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
   })
 }
