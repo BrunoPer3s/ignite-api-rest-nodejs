1c2f2b2a9ced2f5dca20641a7086f75620b130c5
brunoper3s <brunoperes7@hotmail.com>
2023-05-04 12:14:34 -0300

    List Transactions Test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -12,12 +12,39 @@ describe('Transactions routes', () => {
   })
 
   it('should be able to create a new transaction', async () => {
-    const response = await request(app.server).post('/transactions').send({
-      title: 'New Transaction',
-      amount: 5000,
-      type: 'credit',
-    })
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
 
-    expect(response.statusCode).toEqual(201)
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    console.log(listTransactionsResponse.body.transactions)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })
